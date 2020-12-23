db4964286cf6193be74884620388bc22b29c3670
iHromnik <hromnik@me.com>
2020-12-23 11:07:43 +0100

    New project created

---
+++ Swift-VK/Swift-VK/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-VK
+//
+//  Created by HappyRoman on 23/12/2020.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
