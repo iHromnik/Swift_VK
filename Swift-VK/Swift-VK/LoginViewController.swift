6f0a72fc76a55c273365bf4c17278c5dd4f33b48
iHromnik <hromnik@me.com>
2020-12-26 19:23:20 +0100

    home work 2

--- Swift-VK/Swift-VK/LoginViewController.swift
+++ Swift-VK/Swift-VK/LoginViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
-    
     @IBOutlet weak var loginTextView: UITextField!
-    
     @IBOutlet weak var passwordTextView: UITextField!
+   
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+              let checkResult = checkUserData()
+              if !checkResult {
+                  showLoginError()
+              }
+              return checkResult
+          }
+          
+          func checkUserData() -> Bool {
+              guard let login = loginTextView.text,
+                  let password = passwordTextView.text else { return false }
+              
+              if login == "admin" && password == "123" {
+                  return true
+              } else {
+                  return false
+              }
+          }
+          
+          func showLoginError() {
+              let alter = UIAlertController(title: "Ошибка", message: "Введены не верные данные пользователя", preferredStyle: .alert)
+              let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+              alter.addAction(action)
+              present(alter, animated: true, completion: nil)
+          }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShowe(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-                NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
             }
             
            @objc func keyboardWillShowe(notification: Notification){
